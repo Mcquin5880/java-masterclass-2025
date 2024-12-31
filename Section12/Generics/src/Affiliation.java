8ed97984542145092e4bc9b43e381a0944fe04fc
Michael <mquinter88@gmail.com>
2024-12-31 15:06:03 -0500

    wrap up generics

---
+++ Section12/Generics/src/Affiliation.java
@@ -0,0 +1,7 @@
+public record Affiliation(String name, String type, String countryCode) {
+
+    @Override
+    public String toString() {
+        return name + " (" + type + " in " + countryCode + ")";
+    }
+}
